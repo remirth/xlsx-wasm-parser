9a0fa44d85b2519da954d6c0998a39c25516e175
Exsjabe <jakob.bergstrom@explizit.se>
2023-03-24 13:45:07 +0100

    Added some utils and tests

--- wasm/src/utils.rs
+++ wasm/src/utils.rs
@@ -1,3 +1,10 @@
+use std::io::Cursor;
+
+use calamine::{open_workbook_from_rs, DataType, Xlsx};
+use chrono::Utc;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::JsError;
+
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
@@ -7,4 +14,42 @@ pub fn set_panic_hook() {
     // https://github.com/rustwasm/console_error_panic_hook#readme
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
+}
+
+pub fn open_workbook_from_u8(bytes: &[u8]) -> Result<Xlsx<Cursor<&[u8]>>, JsError> {
+    Ok(open_workbook_from_rs(Cursor::new(bytes))?)
+}
+
+fn convert_to_date(cell: &DataType) -> String {
+    let option = cell
+        .as_datetime()
+        .and_then(|dt| dt.and_local_timezone(Utc).earliest())
+        .map(|dt| dt.to_rfc3339_opts(chrono::SecondsFormat::Secs, true));
+
+    match option {
+        Some(date) => date,
+        None => String::from(""),
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+#[serde(untagged)]
+pub enum RowValue {
+    String(String),
+    Number(f64),
+    Boolean(bool),
+    Undefined,
+}
+
+impl From<&DataType> for RowValue {
+    fn from(cell: &DataType) -> Self {
+        match cell {
+            DataType::String(s) => RowValue::String(s.clone()),
+            DataType::Float(f) => RowValue::Number(*f),
+            DataType::Int(i) => RowValue::Number(*i as f64),
+            DataType::Bool(b) => RowValue::Boolean(*b),
+            DataType::DateTime(_) => RowValue::String(convert_to_date(cell)),
+            _ => RowValue::Undefined,
+        }
+    }
 }
--- wasm/tests/node.rs
+++ wasm/tests/node.rs
@@ -1,6 +1,8 @@
 //! Test suite for Node
-
 #![cfg(target_arch = "wasm32")]
+
+#[path = "../src/utils.rs"]
+mod utils;
 
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
@@ -8,6 +10,58 @@ use wasm_bindgen_test::*;
 // wasm_bindgen_test_configure!(run_in_node);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn datatype_string_to_rowvalue() {
+    let str = "Hello World";
+    let cell = calamine::DataType::String(str.to_string());
+    let control_value = utils::RowValue::String(str.to_string());
+
+    let row_value = utils::RowValue::from(&cell);
+    assert_eq!(row_value, control_value);
+}
+
+#[wasm_bindgen_test]
+fn datatype_float_to_rowvalue() {
+    let float = 3.14;
+    let cell = calamine::DataType::Float(float);
+    let control_value = utils::RowValue::Number(float);
+
+    let row_value = utils::RowValue::from(&cell);
+    assert_eq!(row_value, control_value);
+}
+
+#[wasm_bindgen_test]
+fn datatype_int_to_rowvalue() {
+    let int: i64 = 3;
+    let cell = calamine::DataType::Int(int);
+    let control_value = utils::RowValue::Number(int as f64);
+
+    let row_value = utils::RowValue::from(&cell);
+    assert_eq!(row_value, control_value);
+}
+
+#[wasm_bindgen_test]
+fn datatype_date_to_rowvalue() {
+    let date_string = "1999-11-24T00:00:00Z";
+    let date_code = 36488.0;
+    let cell = calamine::DataType::DateTime(date_code);
+    let control_value = utils::RowValue::String(date_string.to_string());
+
+    let row_value = utils::RowValue::from(&cell);
+    assert_eq!(row_value, control_value);
+}
+
+fn datatype_empty_to_rowvalue() {
+    let cell = calamine::DataType::Empty;
+    let control_value = utils::RowValue::Undefined;
+
+    let row_value = utils::RowValue::from(&cell);
+    assert_eq!(row_value, control_value);
+}
+
+fn datatype_error_to_rowvalue() {
+    let cell = calamine::DataType::Error;
+    let control_value = utils::RowValue::Undefined;
+
+    let row_value = utils::RowValue::from(&cell);
+    assert_eq!(row_value, control_value);
 }
