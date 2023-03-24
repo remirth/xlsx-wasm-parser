4ea083d2349e4e047168d3ee57e2d9336877bfe7
Exsjabe <jakob.bergstrom@explizit.se>
2023-03-24 17:07:54 +0100

    Split node from browser

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     dir: "__test__",
+    environmentMatchGlobs: [
+      ["**/browser/**", "jsdom"],
+      ["**/node/**", "node"],
+    ],
     coverage: {
       exclude: ["**/node_modules/**", "**/wasm/**", "**/__test__/**"],
     },
